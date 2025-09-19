1804bd9051e82ef5e03aad7ace16b254e5adcf95
jstakun <jstakun@redhat.com>
2025-09-19 10:34:34 +0200

    cleanup

--- build_push.sh
+++
@@ -1,4 +0,0 @@
-mvn clean package
-podman build -f ./Containerfile.jvm -t quay.io/jstakun/arrow-ai-assistant
-podman push --authfile ~/jstakun-aiassistant-auth.json quay.io/jstakun/arrow-ai-assistant:latest
-
